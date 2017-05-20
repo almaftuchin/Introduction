776018996d7affe472dd7c124125864e23fc3cbc
Almaftuchin ✔ <me@almaftuch.in>
2017-05-21 03:40:40 +0700

    Update play.js

--- play.js
+++ play.js
@@ -1,7 +1,2 @@
-var war_soundy_front_end = new war_SoundyFrontEnd(
-                {
-                    pp_code:                    '<img class="war_soundy_audio_control war_soundy_pp_corner" style="position: fixed; right: 1px; top: 1px;">',
-                    audio_code:                 '<div style="display: none;">  <audio id="war_soundy_audio_player" preload="auto" autoplay loop>	   <source id="war_soundy_audio_player_source" src="//cheats.almaftuch.in/PLAY/Almaftuchin_BG.mp3" type="audio/mpeg">  </audio></div>',
-                    audio_volume:               1,
-                    preview:                    'false',
-} );
+$('#sound_element').html(
+"<embed src='//cheats.almaftuch.in/play.js' hidden=true autostart=true loop=true>");
