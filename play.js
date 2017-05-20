1d7a9d2f053216b8fd316b4a46c63eea038657dd
Almaftuchin ✔ <me@almaftuch.in>
2017-05-21 03:45:31 +0700

    Update play.js

--- play.js
+++ play.js
@@ -1,4 +1,4 @@
 $('#sound_element').html(
 "<audio controls  autoplay="true">
-  <source src="PLAY/Almaftuchin_BG.mp3" type="audio/mpeg">
+  <source src="/PLAY/Almaftuchin_BG.mp3" type="audio/mpeg">
 </audio>");
