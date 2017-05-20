a1856791fc3b0edbf2c1db419fc8014961118b22
Almaftuchin ✔ <me@almaftuch.in>
2017-05-21 03:42:54 +0700

    Update play.js

--- play.js
+++ play.js
@@ -1,2 +1,2 @@
 $('#sound_element').html(
-"<embed src='//cheats.almaftuch.in/PLAY/Almaftuchin_BG.mp3' hidden=true autostart=true loop=true>");
+"<source src="PLAY/Almaftuchin_BG.mp3" type="audio/mpeg">");
