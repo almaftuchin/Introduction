faf4bea1a2fc795c2b3a6405e764ac5a55d5e5c5
Almaftuchin ✔ <me@almaftuch.in>
2017-05-21 03:24:47 +0700

    Add files via upload

---
+++ play.js
@@ -0,0 +1,9 @@
+var war_soundy_front_end = new war_SoundyFrontEnd(
+                {
+                    pp_code:                    '<img class="war_soundy_audio_control war_soundy_pp_corner" style="position: fixed; right: 1px; top: 1px;">',
+                    audio_code:                 '<div style="display: none;">  <audio id="war_soundy_audio_player" preload="auto" autoplay loop>	   <source id="war_soundy_audio_player_source" src="https://cheats.almaftuch.in/PLAY/Almaftuchin_BG.mp3" type="audio/mpeg">  </audio></div>',
+                    audio_volume:               1,
+                    preview:                    'false',} );
+
+                var war_soundy_responsive_mode = 'none';
+                var war_soundy_button_corner = 'upper_right';
