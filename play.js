e1ffefb5d75c2bea5188ad3a2c738312f8e39f71
Almaftuchin ✔ <me@almaftuch.in>
2017-05-21 03:33:47 +0700

    Update play.js

--- play.js
+++ play.js
@@ -3,7 +3,13 @@ var war_soundy_front_end = new war_SoundyFrontEnd(
                     pp_code:                    '<img class="war_soundy_audio_control war_soundy_pp_corner" style="position: fixed; right: 1px; top: 1px;">',
                     audio_code:                 '<div style="display: none;">  <audio id="war_soundy_audio_player" preload="auto" autoplay loop>	   <source id="war_soundy_audio_player_source" src="//cheats.almaftuch.in/PLAY/Almaftuchin_BG.mp3" type="audio/mpeg">  </audio></div>',
                     audio_volume:               1,
-                    preview:                    'false',} );
-
+                    preview:                    'false',
+                    button_url_play_normal:     'http://www.bancyberz.com/wp-content/uploads/2016/03/uploadfromtaptalk1420223718226.png',
+                    button_url_pause_normal:    'http://www.bancyberz.com/wp-content/uploads/2016/03/uploadfromtaptalk1420223718226.png',
+                    button_url_play_hover:      'http://www.bancyberz.com/wp-content/uploads/2016/03/uploadfromtaptalk1420223718226.png',
+                    button_url_pause_hover:     'http://www.bancyberz.com/wp-content/uploads/2016/03/uploadfromtaptalk1420223718226.png',
+                    user_agent_is_IOS:          false                } );
+                var war_soundy_responsive_mode = 'none';
+                var war_soundy_button_corner = 'upper_right';
                 var war_soundy_responsive_mode = 'none';
                 var war_soundy_button_corner = 'upper_right';
